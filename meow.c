ebcb41fbff018ac9b8d20b90ac2562fc065ed53e
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-09 22:54:23 +0000

    Thu, Nov 9, 2023, 3:54 PM -07:00

--- meow.c
+++ meow.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    int counter = 3;
+    int i = 3;
 
-    while (counter > 0)
+    while (i > 0)
     {
         printf("meow\n");
-        counter--;
+        i--;
     }
 }
