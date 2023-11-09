ddbf4c73db9fd166dca7d172bc77bbf6e83f38b1
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-09 23:10:47 +0000

    Thu, Nov 9, 2023, 4:10 PM -07:00

--- meow.c
+++ meow.c
@@ -3,16 +3,18 @@
 int main(void)
 {
     // FOR LOOP
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     printf("meow\n");
-    // }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("meow\n");
+    }
 
     // WHILE LOOP
     // int i = 0;
 
     // If you always want the expression to be true
-    // & run forever simply use boolean true
+    // & run forever simply use boolean true --> while (true)
+    // Any other non-zero value would have same effect
+
     // while (i < 3)
     // {
     //     printf("meow\n");
