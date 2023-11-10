1ffecb2dc96a7932494d7322945f5d17274dd0c3
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-10 23:00:08 +0000

    Fri, Nov 10, 2023, 4:00 PM -07:00

--- 1-pset/population/population.c
+++ 1-pset/population/population.c
@@ -1,13 +1,46 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void)
-{
-    // TODO: Prompt for start size
+// initial population variable intake
+// desired population variable intake
+// years output
 
-    // TODO: Prompt for end size
+// current population variable
+// years variable
 
-    // TODO: Calculate number of years until we reach threshold
+// create loop while below target population
+    // add births
+    // subtract deaths
+    // update current population
+    // increment years
 
-    // TODO: Print number of years
+// print years
+
+int main(void) {
+    //establish initial and target populations
+    int initialPop;
+    do
+    {
+        get_int("Starting population: ");
+    }
+    while (initialPop < 1);
+    int targetPop;
+    do
+    {
+        get_int("Target population: ");
+    }
+    while (targetPop < initialPop);
+
+    // create variables for current population and years
+    int currentPop = initialPop;
+    int years = 0;
+
+    // loop to update current population and increment years
+    while (currentPop < targetPop)
+    {
+        currentPop = currentPop + (currentPop / 3) - (currentPop / 4);
+        years++;
+    }
+
+    printf("Years: %i", years);
 }
