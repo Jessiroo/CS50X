6b31438b57dadeacccbf149b2367296c899abb74
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-10 22:54:04 +0000

    Fri, Nov 10, 2023, 3:54 PM -07:00

--- population.c
+++ population.c
@@ -17,8 +17,25 @@
 // print years
 
 int main(void) {
-    int initialPop = get_int("Starting population: ");
-    int targetPop = get_int("Target population: ");
+    //establish initial and target populations
+    int initialPop;
+    do
+    {
+        get_int("Starting population: ");
+    }
+    while (initialPop < 1);
+    int targetPop;
+    do
+    {
+        get_int("Target population: ");
+    }
+    while (targetPop < initialPop);
 
-    
+    int currentPop = initialPop;
+    int years = 0;
+
+    while (currentPop < targetPop)
+    {
+        
+    }
 }
