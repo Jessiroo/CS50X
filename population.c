4fd1af08b6b310533372ee6f0464138a40bd202c
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-10 22:34:39 +0000

    Fri, Nov 10, 2023, 3:34 PM -07:00

--- population.c
+++ population.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
-// initial population variable
-// desired population variable
-// years output
+// initial population variable intake
+// desired population variable intake
+// years output input
 
-// 
+// current population variable
+
+// create loop while below target 
+    // check cu
