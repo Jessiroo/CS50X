93dc3f47007817aadebe3e7f2b816c62baa1dddb
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-10 23:08:27 +0000

    Fri, Nov 10, 2023, 4:08 PM -07:00

--- 1-pset/population/population.c
+++ 1-pset/population/population.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
 
-// initial population variable intake
-// desired population variable intake
-// years output
-
-// current population variable
-// years variable
-
-// create loop while below target population
-    // add births
-    // subtract deaths
-    // update current population
-    // increment years
-
-// print years
-
 int main(void) {
     //establish initial and target populations
     int initialPop;
