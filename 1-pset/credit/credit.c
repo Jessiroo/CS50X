69f3c683418a43d469c7620242a9ba86daec9dd6
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-22 02:19:05 +0000

    Tue, Nov 21, 2023, 7:19 PM -07:00

--- 1-pset/credit/credit.c
+++ 1-pset/credit/credit.c
@@ -1,30 +1,4 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int get_card(void);
-void check_card(int num);
 
-// main function
-int main(void)
-{
-    const cardNumber = get_card();
-
-}
-
-// get_card function
-int get_card(void)
-{
-    int number;
-    do
-    {
-        number = get_long("Number: ");
-    }
-    while (number.length < 13);
-}
-
-// check_card function
-void check_card(int num)
-{
-    
-    return printf("INVALID\n");
-}
