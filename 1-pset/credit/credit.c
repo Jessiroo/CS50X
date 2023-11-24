8f4b1548385edb14c9060f87f13258d9db07e218
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-24 23:16:36 +0000

    Fri, Nov 24, 2023, 4:16 PM -07:00

--- 1-pset/credit/credit.c
+++ 1-pset/credit/credit.c
@@ -6,5 +6,11 @@ int main(void)
     // get the long
     // create digit pointers
     // number of digits for even/odd
-    // 
+    // sum of odds (multiplied by 2 and re-added)
+    // sum of evens
+
+    long cc_number = get_long("Card Number: ");
+    int digit1 = 0;
+    int digit2 = 0;
+    int number_of_digits
 }
