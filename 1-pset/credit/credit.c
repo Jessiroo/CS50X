a2c38dbecdadab8c1ac0b57a860354367978dda3
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-24 23:22:40 +0000

    Fri, Nov 24, 2023, 4:22 PM -07:00

--- 1-pset/credit/credit.c
+++ 1-pset/credit/credit.c
@@ -12,5 +12,16 @@ int main(void)
     long cc_number = get_long("Card Number: ");
     int digit1 = 0;
     int digit2 = 0;
-    int number_of_digits
+    int number_of_digits = 0;
+    int sum_odds = 0;
+    int sum_evens = 0;
+
+    // create while loop, as long as cc number is greater than 0 (still exists)
+    // update digits, d2 becomes what d1 was, d1 becomes the last digit
+    // if number of digits even
+        // add d1 to the sum
+    // if number of digits odd (else)
+        // create new variable multiplying d1 by 2
+        // add the digit(s) of new multiple together and add to odds sum
+    // 
 }
