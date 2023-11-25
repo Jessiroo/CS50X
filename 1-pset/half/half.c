6969057887ef8251ee3fdab5f43372d5cb1558c0
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-25 00:13:17 +0000

    Fri, Nov 24, 2023, 5:13 PM -07:00

--- 1-pset/half/half.c
+++ 1-pset/half/half.c
@@ -18,7 +18,7 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    float total_bill = bill + (bill * tax);
-    
-    return 0.0;
+    float taxed_bill = bill + (bill * tax);
+    float total_bill = taxed_bill + (taxed_bill * (tip / 100));
+    return total_bill / 2;
 }
