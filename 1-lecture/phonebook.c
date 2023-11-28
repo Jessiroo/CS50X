d8cffff508824324ada9aea029d57ad0c63178bd
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-28 22:46:29 +0000

    Tue, Nov 28, 2023, 3:46 PM -07:00

--- 1-lecture/phonebook.c
+++ 1-lecture/phonebook.c
@@ -2,30 +2,39 @@
 #include <cs50.h>
 #include <string.h>
 
+// MINE
+// int main(void)
+// {
+//     bool correct_info = false;
+
+//     do
+//     {
+//         string name = get_string("Name: ");
+//         string address = get_string("Address: ");
+//         int phone_number = get_int("Phone: ");
+
+//         printf("Your name is %s, address is %s, and phone is %i\n", name, address, phone_number);
+//         printf("Is this correct? y/n");
+
+//         string response = get_string(" ");
+
+//         int result1 = strcmp(response, "y");
+//         int result2 = strcmp(response, "Y");
+
+//         if (result1 == 0 || result2 == 0)
+//         {
+//             correct_info = true;
+//         }
+//     }
+//     while (correct_info == false);
+
+//     printf("Information confirmed.\n");
+// }
+
+// LECTURE:
 int main(void)
 {
-    bool correct_info = false;
-
-    do
-    {
-        string name = get_string("Name: ");
-        string address = get_string("Address: ");
-        int phone_number = get_int("Phone: ");
-
-        printf("Your name is %s, address is %s, and phone is %i\n", name, address, phone_number);
-        printf("Is this correct? y/n");
-
-        string response = get_string(" ");
-
-        int result1 = strcmp(response, "y");
-        int result2 = strcmp(response, "Y");
-
-        if (result1 == 0 || result2 == 0)
-        {
-            correct_info = true;
-        }
-    }
-    while (correct_info == false);
-
-    printf("Information confirmed.\n");
+    string name = get_string("What's your name? ");
+    int age = get_int("What's your age? ");
+    
 }
