f3e57d0075aa78df74ef83470af1020b3eb81999
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-28 22:16:47 +0000

    Tue, Nov 28, 2023, 3:16 PM -07:00

--- 1-lecture/phonebook.c
+++ 1-lecture/phonebook.c
@@ -3,5 +3,9 @@
 
 int main(void)
 {
-    
+    string name = get_string("Name: ");
+    string address = get_string("Address: ");
+    int phone_number = get_int("Phone: ");
+
+    printf("Your name is %s, address is ", name, address, phone_number);
 }
