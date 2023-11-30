1268daa7397564a3c8889daaca9a2fab9759c898
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-11-30 23:26:22 +0000

    Thu, Nov 30, 2023, 4:26 PM -07:00

--- 1-pset/population/population.c
+++ 1-pset/population/population.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// MINE
+// int main(void)
+// {
+//     //establish initial and target populations
+//     int initialPop;
+//     do
+//     {
+//         initialPop = get_int("Starting population: ");
+//     }
+//     while (initialPop < 9);
+
+//     int targetPop;
+//     do
+//     {
+//         targetPop = get_int("Target population: ");
+//     }
+//     while (targetPop < initialPop);
+
+//     // create variables for current population and years
+//     int currentPop = initialPop;
+//     int years = 0;
+
+//     // loop to update current population and increment years
+//     while (currentPop < targetPop)
+//     {
+//         currentPop = currentPop + (currentPop / 3) - (currentPop / 4);
+//         years++;
+//     }
+
+//     printf("Years: %i\n", years);
+// }
+
+// SECTION LECTURE
 int main(void)
 {
-    //establish initial and target populations
-    int initialPop;
-    do
-    {
-        initialPop = get_int("Starting population: ");
-    }
-    while (initialPop < 9);
     
-    int targetPop;
-    do
-    {
-        targetPop = get_int("Target population: ");
-    }
-    while (targetPop < initialPop);
-
-    // create variables for current population and years
-    int currentPop = initialPop;
-    int years = 0;
-
-    // loop to update current population and increment years
-    while (currentPop < targetPop)
-    {
-        currentPop = currentPop + (currentPop / 3) - (currentPop / 4);
-        years++;
-    }
-
-    printf("Years: %i\n", years);
 }
