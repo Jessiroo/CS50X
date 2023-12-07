3b2bea37df46e5405f4336a933752a5ab30818f8
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-12-07 23:19:10 +0000

    Thu, Dec 7, 2023, 4:19 PM -07:00

---
+++ 2-lecture/scores.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Declaring and Using Arrays
+int main(void)
+{
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) 3);
+}
+// with this approach memory is set aside ahead of time for an array
+// of three scores which are integers
+// memory is only used to keep track of one variable name
+
+// Declaring individual variables
+// int main(void)
+// {
+//     int score1 = 72;
+//     int score2 = 73;
+//     int score3 = 33;
+
+//     printf("Average: %f\n", (score1 + score2 + score3) / (float) 3);
+// }
+// In this method the memory must be set aside for each
+// individual variable name AND integer
