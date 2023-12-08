5918947ef2947a17e3b690c7af4790aa5b6f72b3
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-12-08 00:06:27 +0000

    Thu, Dec 7, 2023, 5:06 PM -07:00

--- 2-lecture/hi.c
+++ 2-lecture/hi.c
@@ -1,1 +1,11 @@
-#include <stdio.h?
+#include <stdio.h>
+
+// DEFINING INDIVIDUAL CHARACTERS INSTEAD OF STRING:
+int main(void)
+{
+    char c1 = "H";
+    char c2 = "I";
+    char c3 = "!";
+
+    printf("%c%c%c\n", c1, c2, c3);
+}
