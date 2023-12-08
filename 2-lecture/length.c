dab41c7c78759168909c1ae111a4869ab0671acd
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-12-08 00:57:57 +0000

    Thu, Dec 7, 2023, 5:57 PM -07:00

--- 2-lecture/length.c
+++ 2-lecture/length.c
@@ -1,15 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
+// USING STRING LIBRARY
 int main(void)
 {
     string name = get_string("What's your name? ");
-
-    int n = 0;
-    while (name[n] != '\0')
-    {
-        n++;
-    }
-
+    int n = strlen(name);
     printf("Your name is %i characters long.\n", n);
 }
+
+
+
+// MANUALLY DETERMINE STRING LENGTH
+// int main(void)
+// {
+//     string name = get_string("What's your name? ");
+
+//     int n = 0;
+//     while (name[n] != '\0')
+//     {
+//         n++;
+//     }
+
+//     printf("Your name is %i characters long.\n", n);
+// }
