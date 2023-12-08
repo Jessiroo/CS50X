772b7c160b20ec7193140f53160b7e7e19dfdb0e
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-12-08 23:43:05 +0000

    Fri, Dec 8, 2023, 4:43 PM -07:00

--- 2-pset/array.c
+++ 2-pset/array.c
@@ -7,7 +7,8 @@ int main(void)
 
     int array[length];
 
-    for (int i = 0, int value = 1; i < length; i++)
+    int value = 1;
+    for (int i = 0; i < length; i++)
     {
         array[i] = value;
         value = value x 2;
