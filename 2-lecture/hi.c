4ec9e63718a596650e1ae4c60f0b50ad5f0d41d9
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-12-08 00:22:51 +0000

    Thu, Dec 7, 2023, 5:22 PM -07:00

--- 2-lecture/hi.c
+++ 2-lecture/hi.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <cs50.h>
 
-// DEFINING AS A STRING (at this point with the cs50 library)
+// DEFINING AN ARRAY OF STRINGS
+// as strings can be variable in length, the NUL byte at the end of the string
+// is important for defining the end of one and beginning of the next
 int main(void)
 {
-    string s = "HI!";
+    string words[2];
 
-    printf("%s\n", s);
-    printf("%c %c %c\n", s[0], s[1], s[2]);
-    printf("%i %i %i\n", s[0], s[1], s[2]);
+    words[0] = "HI!";
+    words[1] = "BYE!";
+
+    printf("%s\n", words[0]);
+    printf("%s\n", words[1]);
 }
+
+
+
+// DEFINING AS A STRING (at this point with the cs50 library)
+// int main(void)
+// {
+//     string s = "HI!";
+
+//     printf("%s\n", s);
+//     printf("%c %c %c\n", s[0], s[1], s[2]);
+//     printf("%i %i %i\n", s[0], s[1], s[2]);
+// }
 
 
 
