1368edcee25e59a44a19a87fdd1fe615f848bc01
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-12-08 22:58:46 +0000

    Fri, Dec 8, 2023, 3:58 PM -07:00

--- 2-lecture/status.c
+++ 2-lecture/status.c
@@ -1,14 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// command line cue for checking exit status: echo $?
+
 int main(int argc, string argv[])
 {
     if (argc != 2)
     {
         printf("Missing command-line argument\n");
+        return 1;
+        // here 1 serves as the return statement for int main
+        // and would be a specific error code for this case
     }
     else
     {
         printf("Hello, %s\n", argv[1]);
+        return 0;
+        // return 0 is not actually necessary here
+        // because by default the return value is 0
     }
 }
