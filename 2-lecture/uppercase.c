ca7f283094a6d8378102c0ca5beb39934482299a
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-12-08 01:16:29 +0000

    Thu, Dec 7, 2023, 6:16 PM -07:00

--- 2-lecture/uppercase.c
+++ 2-lecture/uppercase.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
+// USING ctype.h LIBRARY
 int main(void)
 {
     string s = get_string("Before: ");
@@ -9,14 +11,29 @@ int main(void)
 
     for (int i = 0; i < strlen(s); i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            printf("%c", s[i] - 32);
-        }
-        else
-        {
-            printf("%c", s[i]);
-        }
+        printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
+
+
+
+// MANUALLY TRANSITION LOWERCASE TO UPPERCASE
+// int main(void)
+// {
+//     string s = get_string("Before: ");
+//     printf("After: ");
+
+//     for (int i = 0; i < strlen(s); i++)
+//     {
+//         if (s[i] >= 'a' && s[i] <= 'z')
+//         {
+//             printf("%c", s[i] - 32);
+//         }
+//         else
+//         {
+//             printf("%c", s[i]);
+//         }
+//     }
+//     printf("\n");
+// }
