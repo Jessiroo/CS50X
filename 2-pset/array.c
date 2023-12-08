e87ebe0e35dcb84404db3f6b4699248c6724ef6f
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2023-12-08 23:42:10 +0000

    Fri, Dec 8, 2023, 4:42 PM -07:00

---
+++ 2-pset/array.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int length = get_int("What is the length of the array? ");
+
+    int array[length];
+
+    for (int i = 0, int value = 1; i < length; i++)
+    {
+        array[i] = value;
+        value = value x 2;
+    }
+}
