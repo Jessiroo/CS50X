b0cc1ff75bcb2bd203d5c2917a58cba5d5ffc281
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2024-01-24 22:11:45 +0000

    Wed, Jan 24, 2024, 3:11 PM -07:00

--- 2-pset/no-vowels.c
+++ 2-pset/no-vowels.c
@@ -1,8 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+string replace(string input[]);
+
+// main program
 int main(int argc, string argv[])
 {
     int length = argc;
-    
+
+}
+
+// vowel replacer function
+string replace(string input[])
+{
+
 }
