d91babe8c0480e45d56420d5fe151f0d62f3506a
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2024-01-24 01:55:17 +0000

    Tue, Jan 23, 2024, 6:55 PM -07:00

--- 2-pset/hours.c
+++ 2-pset/hours.c
@@ -57,7 +57,7 @@ float calc_hours(int hours[], int weeks, char output_type)
     }
     else
     {
-        float average = float sum / weeks;
+        float average = (float) sum / weeks;
         return average;
     }
 }
