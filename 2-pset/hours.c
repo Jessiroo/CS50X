4758886a2809b00ba18b272dbf2e307834e4ff91
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2024-01-24 01:15:28 +0000

    Tue, Jan 23, 2024, 6:15 PM -07:00

---
+++ 2-pset/hours.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // request number of weeks taking CS50
+        // do while number is 0 or less
+    // create array of length weeks
+
+    // iterate over weeks requesting hours per week until all weeks done
+
+    // ask user for A or T to calculate total or average
+    // print answer
+}
