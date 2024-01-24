5c3ea3395c34eca69c46f1834bb0f5b80a99c22f
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2024-01-24 22:51:22 +0000

    Wed, Jan 24, 2024, 3:51 PM -07:00

--- 2-pset/no-vowels.c
+++ 2-pset/no-vowels.c
@@ -13,42 +13,42 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    string input = argv[1];
+    printf("%s\n", argv[1]);
 
-    string answer = replace(input);
+    // string answer = replace(input);
 
-    printf("%s/n", answer);
+    // printf("%s/n", answer);
 }
 
 // vowel replacer function
-string replace(string input)
-{
-    int length = strlen(input);
-    string new_string[length];
+// string replace(string input)
+// {
+//     int length = strlen(input);
+//     string new_string[length];
 
-    for(int i = 0; i < length; i++)
-    {
-        if (input[i] == 'a' || input[i] == 'A')
-        {
-            new_string[i] = 6;
-        }
-        else if (input[i] == 'e' || input[i] == 'E')
-        {
-            new_string[i] = 3;
-        }
-        else if (input[i] == 'i' || input[i] == 'I')
-        {
-            new_string[i] = 1;
-        }
-        else if (input[i] == 'o' || input[i] == 'O')
-        {
-            new_string[i] = 0;
-        }
-        else
-        {
-            new_string[i] = input[i];
-        }
+//     for(int i = 0; i < length; i++)
+//     {
+//         if (input[i] == 'a' || input[i] == 'A')
+//         {
+//             new_string[i] = 6;
+//         }
+//         else if (input[i] == 'e' || input[i] == 'E')
+//         {
+//             new_string[i] = 3;
+//         }
+//         else if (input[i] == 'i' || input[i] == 'I')
+//         {
+//             new_string[i] = 1;
+//         }
+//         else if (input[i] == 'o' || input[i] == 'O')
+//         {
+//             new_string[i] = 0;
+//         }
+//         else
+//         {
+//             new_string[i] = input[i];
+//         }
 
-        return new_string;
-    }
-}
+//         return new_string;
+//     }
+// }
