799b4a19d78d5eb846f850b98b78146bd4ceb4fe
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2024-01-24 22:17:33 +0000

    Wed, Jan 24, 2024, 3:17 PM -07:00

--- 2-pset/no-vowels.c
+++ 2-pset/no-vowels.c
@@ -6,11 +6,22 @@ string replace(string input[]);
 // main program
 int main(int argc, string argv[])
 {
-    
+    if (argc > 2)
+    {
+        return;
+    }
+
+    replace(argv[]);
 }
 
 // vowel replacer function
 string replace(string input[])
 {
+    int length = strlen(input[]);
+    string new_string[length];
 
+    for(int i = 0; i < length; i++)
+    {
+        if ()
+    }
 }
