c1a01b7ad435dcad2a1fd6de9ccb66531a7b000f
Jessiroo <134339619+Jessiroo@users.noreply.github.com>
2024-01-25 23:27:42 +0000

    Thu, Jan 25, 2024, 4:27 PM -07:00

--- 2-pset/scrabble/scrabble.c
+++ 2-pset/scrabble/scrabble.c
@@ -14,8 +14,6 @@ int main(void)
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
 
-    
-
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
